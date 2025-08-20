764d15b93f30d2a373805a69d5df7baf09c50e61
Amr Waheed <amrmohamed877@gmail.com>
2025-08-20 14:35:53 +0300

    first commit

---
+++ app/services/api.js
@@ -0,0 +1,13 @@
+export async function fetchProducts() {
+    try {
+        const response = await fetch('https://dummyjson.com/products?limit=194');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Failed to fetch products:", error);
+    }
+}
+
