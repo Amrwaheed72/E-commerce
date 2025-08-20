8e9038b3b2ec749ea53b127ede2668b4f3f91593
Amr Waheed <amrmohamed877@gmail.com>
2025-08-20 15:10:12 +0300

    setting up

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import { fetchProducts } from "./services/api";
+import Image from 'next/image';
+import { fetchProducts } from './services/api';
 
 export default async function Home() {
-
-  const products = await fetchProducts ()
-  console.log(products)
+  const products = await fetchProducts();
+  console.log(products);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -18,7 +17,7 @@ export default async function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.js
             </code>
